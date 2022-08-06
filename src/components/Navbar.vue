fc3d4bf8b7d8d59c2d909e53e475d947c50570c4
boski <boskitechh@gmail.com>
2022-08-07 00:51:24 +0100

    Added mobile navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,57 +1,97 @@
 <template>
-    <v-app-bar height="90px" style="background-color:transparent"
-        :class="{ 'setScrollColor': scrolled, 'white': scrolled }" elevate-on-scroll fixed class="nav-appbar">
-        <div class="navbar-body d-flex flex-row">
-            <img @click="$router.push('/')" class="navbar-image" src="../assets/img/Roadrunner-Logo.svg">
-            <v-spacer></v-spacer>
-            <v-btn class="d-flex d-sm-none" icon>
-                <v-icon>mdi-menu</v-icon>
-            </v-btn>
-            <div class="navbar-buttons mt-4 d-none d-md-flex">
-                <v-btn @click="$router.push('/howitworks')" x-large text>
-                    How it works
+    <div>
+        <v-app-bar height="90px" style="background-color:transparent"
+            :class="{ 'setScrollColor': scrolled, 'white': scrolled }" elevate-on-scroll fixed class="nav-appbar">
+            <div class="navbar-body d-flex flex-row">
+                <img @click="$router.push('/')" class="navbar-image" src="../assets/img/Roadrunner-Logo.svg">
+                <v-spacer></v-spacer>
+                <v-btn @click="drawer = true" class="d-flex d-sm-none" icon>
+                    <v-icon>mdi-menu</v-icon>
                 </v-btn>
-                <v-menu rounded offset-y>
-                    <template v-slot:activator="{ attrs, on }">
-                        <v-btn v-bind="attrs" v-on="on" x-large text>
-                            Company<v-icon>mdi-chevron-down</v-icon>
-                        </v-btn>
-                    </template>
-                    <v-list>
-                        <v-list-item link to="/about">
-                            <v-list-item-title>About</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item link to="/contactus">
-                            <v-list-item-title>Contact Us</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-                <v-btn @click="$router.push('/finance')" x-large text>
-                    Finance
-                </v-btn>
-                <v-btn @click="$router.push('/faqs')" x-large text>
-                    FAQs
-                </v-btn>
-                <v-btn @click="$router.push('/signup')" color="#042C5B"
-                    style="margin-right:20px; border: 2px solid #042C5B; font-weight: 500; width:160px;" x-large
-                    outlined>
-                    Log In
-                </v-btn>
+                <div class="navbar-buttons mt-4 d-none d-md-flex">
+                    <v-btn @click="$router.push('/howitworks')" x-large text>
+                        How it works
+                    </v-btn>
+                    <v-menu rounded offset-y>
+                        <template v-slot:activator="{ attrs, on }">
+                            <v-btn v-bind="attrs" v-on="on" x-large text>
+                                Company<v-icon>mdi-chevron-down</v-icon>
+                            </v-btn>
+                        </template>
+                        <v-list>
+                            <v-list-item link to="/about">
+                                <v-list-item-title>About</v-list-item-title>
+                            </v-list-item>
+                            <v-list-item link to="/contactus">
+                                <v-list-item-title>Contact Us</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                    <v-btn @click="$router.push('/finance')" x-large text>
+                        Finance
+                    </v-btn>
+                    <v-btn @click="$router.push('/faqs')" x-large text>
+                        FAQs
+                    </v-btn>
+                    <v-btn @click="$router.push('/signup')" color="#042C5B"
+                        style="margin-right:20px; border: 2px solid #042C5B; font-weight: 500; width:160px;" x-large
+                        outlined>
+                        Log In
+                    </v-btn>
 
-                <v-btn @click="$router.push('/signup')" color="#042C5B"
-                    style="color:white; font-weight: 500; width:160px" x-large>
-                    Sign Up
-                </v-btn>
+                    <v-btn @click="$router.push('/signup')" color="#042C5B"
+                        style="color:white; font-weight: 500; width:160px" x-large>
+                        Sign Up
+                    </v-btn>
+                </div>
             </div>
-        </div>
-    </v-app-bar>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <div class="d-flex flex-row justify-center">
+                <img @click="$router.push('/')" style="width:70px; " src="../assets/img/Roadrunner-Logo.svg">
+            </div>
+            <v-list nav dense class="px-0">
+                <v-list-item-group active-class="deep-blue--text text--white-4">
+                    <v-list-item link to="/howitworks" class=" py-0">
+                        <v-list-item-title>How It Works</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                    <v-list-item>
+                        <v-list-item-title>Company</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+
+                    <v-list-item link to="/finance">
+                        <v-list-item-title>Finance</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+
+                    <v-list-item link to="faqs">
+                        <v-list-item-title>FAQs</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+
+                    <v-list-item link to="/signup">
+                        <v-list-item-title>Login</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+
+                    <v-list-item link to="signup">
+                        <v-list-item-title>Sign Up</v-list-item-title>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Navbar',
     data: () => ({
-        scrolled: false
+        scrolled: false,
+        drawer: false,
     }),
     methods: {
         handleScroll() {
